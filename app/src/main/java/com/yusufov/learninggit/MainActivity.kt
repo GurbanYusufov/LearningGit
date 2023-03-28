f23f25cf6df4f144c9c083fe4058dd5a7cc8b951
Qurban Yusufov <qurbanyusifov89@gmail.com>
2023-03-28 12:31:24 +0400

    Number count process finished

--- app/src/main/java/com/yusufov/learninggit/MainActivity.kt
+++ app/src/main/java/com/yusufov/learninggit/MainActivity.kt
@@ -2,13 +2,21 @@ package com.yusufov.learninggit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+    private var number:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("Cefer")
         println("Qurban")
+    }
+
+    fun btn_add(view:View){
+        findViewById<AppCompatButton>(R.id.txt_number).setText(number)
+        number+=1
     }
     //Comment2
 }
